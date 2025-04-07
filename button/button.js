72420277017c9cd59ee77d311d21788e67a70da0
Zhikang-Rong <116642864+Zhikang-Rong@users.noreply.github.com>
2025-04-07 15:48:59 +0200

    2025/04/07 update

--- button/button.js
+++ button/button.js
@@ -1,101 +1,250 @@
-    /*
-    if(browser.storage.local.get("btns") === null){
-        browser.storage.local.set({"btns":0});
-    }
-    if(browser.storage.local.get("colorArrow") === null){
-        browser.storage.local.set({"colorArrow":"black"});
-    }
-    if(browser.storage.local.get("colorBg") === null){
-        browser.storage.local.set({"colorBg":"white"});
-    }
-    if(browser.storage.local.get("positionBtn") === null){
-        browser.storage.local.set({"positionBn":"bottomRight"});
-    }*/
-    
-    console.log("button.js");
-    
 
-    function insertButton() {
-        removeExistingButton();
-        const button = document.createElement("button");
-        let buttonIcon = document.createElement("img");
-        buttonIcon.setAttribute("src",chrome.runtime.getURL("assets/icons/arrow-black-48.png"));
+    function setPosition(btns, positionBtn, sizeBtn){
+      let style;
+      if(btns == "0" || btns == "1"){
+        switch(positionBtn){
+          case "topLeft":
+            style = 'left: 30px; top:20px;';
+            break;
+          case "topCenter":
+            style = 'left: 50%; top:20px; transform: translate(-50%,0);';
+            break;
+          case "topRight":
+            style = 'right: 30px; top:20px;';
+            break;
+
+          case "centerLeft":
+            style = 'left: 30px; top:50%; transform: translate(0,-50%);';
+            break;
+          case "center":
+            style = 'left: 50%; top:50%; transform: translate(-50%,-50%);';
+            break;
+          case "centerRight":
+            style = 'right: 30px; top:50% ; transform: translate(0,-50%);';
+            break;
+
+          case "bottomLeft":
+            style = 'left: 30px; bottom:20px;';
+            break;
+          case "bottomCenter":
+            style = 'left: 50%; bottom:20px; transform: translate(-50%,0);';
+            break;
+          case "bottomRight":
+            style = 'right: 30px; bottom:20px;';
+            break;
+        }
+        return style;
+      }else{
+        let style2;
+        switch(positionBtn){
+          case "topLeft":
+            style = 'left: 30px; top:20px;';
+            style2 = 'left: 30px; top:'+(20+parseInt(sizeBtn))+'px;';
+            break;
+          case "topCenter":
+            style = 'left: 50%; top:20px; transform: translate(-50%,0);';
+            style2 = 'left: 50%; top:'+(20+parseInt(sizeBtn))+'px; transform: translate(-50%,0);';
+            break;
+          case "topRight":
+            style = 'right: 30px; top:20px;';
+            style2 = 'right: 30px; top:'+(20+parseInt(sizeBtn))+'px;';
+            break;
+            
+          case "centerLeft":
+            style = 'left: 30px; bottom:50%;';
+            style2 = 'left: 30px; top:50%;';
+            break;
+          case "center":
+            style = 'left: 50%; bottom:50%; transform: translate(-50%,0);';
+            style2 = 'left: 50%; top:50%; transform: translate(-50%,0);';
+            break;
+          case "centerRight":
+            style = 'right: 30px; bottom:50% ;';
+            style2 = 'right: 30px; top:50%;';
+            break;
+
+          case "bottomLeft":
+            style = 'left: 30px; bottom:'+(20+parseInt(sizeBtn))+'px;';
+            style2 = 'left: 30px; bottom:20px;';
+            break;
+          case "bottomCenter":
+            style = 'left: 50%; bottom:'+(20+parseInt(sizeBtn))+'px; transform: translate(-50%,0);';
+            style2 = 'left: 50%; bottom:20px; transform: translate(-50%,0);';
+            break;
+          case "bottomRight":
+            style = 'right: 30px; bottom:'+(20+parseInt(sizeBtn))+'px;';
+            style2 = 'right: 30px; bottom:20px;';
+            break;
+        }
+        return [style,style2];
+      }
+    }
+
+    function setImage(sizeBtn,colorArrow){
+      if(colorArrow == "black"){
+        switch(sizeBtn){
+          case "64":
+            return "assets/icons/arrow-black-64.png";
+          case "32":
+            return "assets/icons/arrow-black-32.png";
+          default:
+            return "assets/icons/arrow-black-48.png";
+        }
+      }else{
+        switch(sizeBtn){
+          case "64":
+            return "assets/icons/arrow-white-64.png";
+          case "32":
+            return "assets/icons/arrow-white-32.png";
+          default:
+            return "assets/icons/arrow-white-48.png";
+        }
+      }
+      
+    }
+
+    function insertButton(result) {
+      removeExistingButton();
+      let btns = result.config2t2b.btns;
+      let colorArrow = result.config2t2b.colorArrow;
+      let colorBg = result.config2t2b.colorBg;
+      let positionBtn = result.config2t2b.positionBtn;
+      let sizeBtn = result.config2t2b.sizeBtn;      
         
+      const button = document.createElement("button");
+      let buttonIcon = document.createElement("img");
+      let style = 'box-sizing: border-box;z-index: 999999999; position: fixed !important;padding: 0 !important;border: 0 !important;border-radius: 0 !important;';
+        
+      const button2 = document.createElement("button");
+      let style2 = 'box-sizing: border-box;z-index: 999999999; position: fixed !important;padding: 0 !important;border: 0 !important;border-radius 0 !important;';
+      let buttonIcon2 = document.createElement("img");
+
+      //Set button position
+      if(btns == "0"){
+        style += setPosition(btns,positionBtn,sizeBtn);
+      }else if(btns == "1"){
+        style2 += setPosition(btns,positionBtn,sizeBtn);
+      }else{
+        let positions = setPosition(btns,positionBtn,sizeBtn);
+        style += positions[0];
+        style2 += positions[1];
+      }
+
+      //Set button size
+      switch(sizeBtn){
+          case "32":
+            style += 'width: 32px; height: 32px;';
+            style2 += 'width: 32px; height: 32px;';
+            break;
+          case "64":
+            style += 'width: 64px; height: 64px;';
+            style2 += 'width: 64px; height: 64px;';
+            break;
+          default:
+            style += 'width: 48px; height: 48px;';
+            style2 += 'width: 48px; height: 48px;';
+      }
+
+      //Set arrow color
+      buttonIcon.setAttribute("src",chrome.runtime.getURL(setImage(sizeBtn,colorArrow)));
+      buttonIcon2.setAttribute("src",chrome.runtime.getURL(setImage(sizeBtn,colorArrow)));
+
+      //Set button color
+      switch(colorBg){
+          case "white":
+            style += "background-color: white;";
+            style2 += "background-color: white;";
+            break;
+          case "black":
+            style += "background-color: black;";
+            style2 += "background-color: black;";
+            break;
+          case "blue":
+            style += "background-color: blue;";
+            style2 += "background-color: blue;";
+            break;
+          case "red":
+            style += "background-color: red;";
+            style2 += "background-color: red;";
+            break;
+          case "green":
+            style += "background-color: green;";
+            style2 += "background-color: green;";
+            break;
+      }
+          
+      buttonIcon.setAttribute('style','width: '+parseInt(sizeBtn)+'px; height: '+parseInt(sizeBtn)+'px;');
+      buttonIcon2.setAttribute('style','width: '+parseInt(sizeBtn)+'px; height: '+parseInt(sizeBtn)+'px;transform: rotate(180deg);');
+        
+      if(btns == "0" || btns == "2"){
         button.appendChild(buttonIcon);
-        buttonIcon.setAttribute('style','width: 48px; height: 48px;');
         button.className = "2t2b-button";
         button.id = "2t2b-top";
-        button.setAttribute('style','box-sizing: border-box;z-index: 999999999; position: fixed; bottom: 70px; right: 30px;width: 50px; height: 50px;border: 1px solid black;border-radius: 0;padding: 0;background-color: white;');
+        button.setAttribute('style',style);
         button.addEventListener("click", function(){
           document.body.scrollTop = 0;
           document.documentElement.scrollTop = 0;
-          });
+        });
         document.body.appendChild(button);
-
-
-        const button2 = document.createElement("button");
-        let buttonIcon2 = document.createElement("img");
-        buttonIcon2.setAttribute("src",chrome.runtime.getURL("assets/icons/arrow-black-48.png"));
-        buttonIcon2.setAttribute('style','width: 48px; height: 48px;transform: rotate(180deg);');
+      }
+      if(btns == "1"|| btns == "2"){
         button2.appendChild(buttonIcon2);
         button2.className = "2t2b-button";
         button2.id = "2t2b-bottom";
-        button2.setAttribute('style','box-sizing: border-box;z-index: 999999999; position: fixed; bottom: 20px; right: 30px;width: 50px; height: 50px;border: 1px solid black;border-radius: 0;padding: 0;background-color: white;');
+        button2.setAttribute('style',style2);
         button2.addEventListener("click", function(){
           document.body.scrollTop = document.body.scrollHeight;
-          document.documentElement.scrollTop = document.documentElement.scrollHeight;
-          });
+          document.documentElement.scrollTop = document.body.scrollHeight;
+        });
         document.body.appendChild(button2);
-        }
-
-    function removeExistingButton() {
-        const existingButton = document.getElementsByClassName("2t2b-button");
-        
-        for (const button of existingButton) {
-          button.remove();
-        }
+      }
     }
 
+    function removeExistingButton() {
+      let element = document.getElementById("2t2b-top");
+      if(element != null)
+        element.remove();
+      element = document.getElementById("2t2b-bottom");
+      if(element != null)
+        element.remove();
+            
+    }
     
-
-    
-
-    /*
-    browser.runtime.onMessage.addListener((message) => {
-        if (message.command === "beastify") {
-          insertBeast(message.beastURL);
-        } else if (message.command === "reset") {
-          removeExistingBeasts();
-        }
-      });*/
-
-insertButton();
-
-
-function gotKitten(item) {
-  console.log(item.config2t2b.btns);
-}
-
-browser.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log("received");
+chrome.storage.local.get("config2t2b", function(result){
+  if(chrome.runtime.lastError || result.config2t2b == undefined){
     let config2t2b = {
-      btns: request.btns,
-      colorArrow: request.colorArrow,
-      colorBg: request.colorBg,
-      positionBtn: request.positionBtn,
-      sizeBtn: request.sizeBtn
+      btns: "0",
+      colorArrow: "black",
+      colorBg: "white",
+      positionBtn: "bottomRight",
+      sizeBtn: "48"
     };
-    
-    browser.storage.local.set({config2t2b});
+    chrome.storage.local.set({config2t2b});
+    chrome.storage.local.get("config2t2b")
+    .then((result) => insertButton(result));
+  }else{
+    insertButton(result);
+  }
+  return;
+});
 
-    let button = browser.storage.local.get("config2t2b");
-    //console.log(`${button.item.config2t2b.btns}`);
-    
-    browser.storage.local.get("config2t2b").then(gotKitten);
-    
-      if( request.message === "start" ) {
-        insertButton();
-      }
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.subject == "submit"){
+      let config2t2b = {
+        btns: request.btns,
+        colorArrow: request.colorArrow,
+        colorBg: request.colorBg,
+        positionBtn: request.positionBtn,
+        sizeBtn: request.sizeBtn
+      };
+      
+      chrome.storage.local.set({config2t2b});
+      
+      chrome.storage.local.get("config2t2b", function(result){
+        insertButton(result);
+        return;
+      });
+    }
   }
 );
--- popup/options.js
+++ popup/options.js
@@ -1,42 +1,52 @@
 
 function optionsSubmit() {
     var btns = document.getElementById("optionsForm").elements.namedItem("btns").value;
-    if(browser.storage.local.get("btns") === null){
-      btns = 0;
-    }
-    console.log(btns);
 
     var colorArrow = document.getElementById("optionsForm").elements.namedItem("colorArrow").value;
-    if(browser.storage.local.get("colorArrow") === null){
-      colorArrow = "black";
-    }
-    console.log(colorArrow);
 
     var colorBg = document.getElementById("optionsForm").elements.namedItem("colorBg").value;
-    if(browser.storage.local.get("colorBg") === null){
-      colorBg = "white";
-    }
-    console.log(colorBg);
 
-    var posBtn = document.getElementById("optionsForm").elements.namedItem("positionBtn").value;
-    if(browser.storage.local.get("positionBtn") === null){
-      posBtn = "bottomRight";
-    }
-    console.log(posBtn);
+    var positionBtn = document.getElementById("optionsForm").elements.namedItem("positionBtn").value;
 
     var sizeBtn = document.getElementById("optionsForm").elements.namedItem("sizeBtn").value;
-    if(browser.storage.local.get("sizeBtn") === null){
-      sizeBtn = "48px";
-    }
-    console.log(posBtn);
 
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"btns": 0, "colorArrow": colorArrow, "colorBg": colorBg, "positionBtn": posBtn, "sizeBtn": sizeBtn});
-      console.log("sent");
-  });
+      chrome.tabs.sendMessage(tabs[0].id, {subject:"submit","btns": btns, "colorArrow": colorArrow, "colorBg": colorBg, "positionBtn": positionBtn, "sizeBtn": sizeBtn});
+    });
+
+    let config2t2b = {
+      btns: btns,
+      colorArrow: colorArrow,
+      colorBg: colorBg,
+      positionBtn: positionBtn,
+      sizeBtn: sizeBtn
+    };
+    chrome.storage.local.set({config2t2b});
 }
 
 document.getElementById("optionsForm").addEventListener("submit", ()=>{
   optionsSubmit();
+});
+chrome.storage.local.get("config2t2b", function(result){
+  if(chrome.runtime.lastError || result.config2t2b == undefined){
+    let config2t2b = {
+      btns: "0",
+      colorArrow: "black",
+      colorBg: "white",
+      positionBtn: "bottomRight",
+      sizeBtn: "48"
+    };
+    chrome.storage.local.set({config2t2b});
+  }
+  return;
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.local.get("config2t2b",function(result){
+    document.getElementById('btns').value = result.config2t2b.btns;
+    document.getElementById('colorArrow').value = result.config2t2b.colorArrow;
+    document.getElementById('colorBg').value = result.config2t2b.colorBg;
+    document.getElementById('positionBtn').value = result.config2t2b.positionBtn;
+    document.getElementById('sizeBtn').value = result.config2t2b.sizeBtn;
+  });
 });
